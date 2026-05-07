3bcb11071ed79fde8dd403868c07b38c6630d4fd
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-05-07 16:14:58 +0400

    config : resolved next.config.ts turbopack root error

--- frontend/src/app/auth/sign/page.tsx
+++ frontend/src/app/auth/sign/page.tsx
@@ -1,8 +1,38 @@
+export const metadata = {
+    title : 'ProporsalAI - Sign Up'
+};
+
 export default function Sign() {
+
+    const SubmitData = () => {
+        try{
+            
+        }catch(err){
+
+        };
+    };
+
     return(
         <div className="auth-container">
-            <div className="auth-start"></div>
-            <div className="auth-end"></div>
+
+            <div className="auth-start">
+
+                <div className="auth-row">
+                    <h2>Win more Upwork clients with AI</h2>
+                    <h6>Generate tailored, high-converting proposals in seconds. Join thousands of freelancers already landing more jobs.</h6>
+                </div>
+
+                <div className="auth-row"></div>
+
+                <div className="auth-row"></div>
+
+            </div>
+
+            <div className="auth-end">
+                <form>
+
+                </form>
+            </div>
         </div>
     );
 };
